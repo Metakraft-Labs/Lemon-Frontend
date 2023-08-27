eef87844159879aa34798155ab1b900e0b8fca7c
Lokendra Soni <lokendrasoni10@gmail.com>
2023-08-27 14:36:46 +0530

    feat: added ai and metaverse details

--- src/pages/Ai/Details/index.js
+++ src/pages/Ai/Details/index.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import { Box, CircularProgress, Typography } from "@mui/material";
+import React, { useCallback, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { get } from "../../../apis/entities";
+import Footer from "../../../components/Footer";
+import Title from "../../../shared/Title";
 
 export default function AiDetails() {
-    return (
-        <div>AiDetails</div>
-    )
+  const [ai, setAi] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const fetchAi = useCallback(async () => {
+    const entity = await get({ id });
+
+    if (!entity) {
+      return navigate("/error-404");
+    }
+
+    setAi(entity);
+  }, [id, navigate]);
+
+  useState(() => {
+    fetchAi();
+  }, [fetchAi]);
+
+  return (
+    <>
+      <Title title={"ai"} />
+      {!ai ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Box
+            sx={{
+              width: "100%",
+              height: "500px",
+              background: `url(${ai?.thumbnail})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "100% 500px",
+              backgroundPosition: "center",
+            }}
+          >
+            <Box
+              sx={{
+                background: "rgba(0,0,0,0.3)",
+                width: "100%",
+                height: "100%",
+              }}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              flexDirection={"column"}
+              gap={"50px"}
+            >
+              <Typography
+                textAlign={"center"}
+                variant="h1"
+                color={"#FFFFFF"}
+                fontWeight={"600"}
+              >
+                {ai?.name}
+              </Typography>
+            </Box>
+          </Box>
+          <Box p={"30px"}>
+            <Typography variant="h3">Description</Typography>
+            <Typography
+              variant="h6"
+              dangerouslySetInnerHTML={{ __html: ai?.description }}
+            />
+          </Box>
+          <Box borderTop={"1px solid black"}>
+            <Footer />
+          </Box>
+        </>
+      )}
+    </>
+  );
 }
--- src/pages/Metaverse/Details/index.js
+++ src/pages/Metaverse/Details/index.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import { Box, CircularProgress, Typography } from "@mui/material";
+import React, { useCallback, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { get } from "../../../apis/entities";
+import Footer from "../../../components/Footer";
+import Title from "../../../shared/Title";
 
 export default function MetaverseDetails() {
-    return (
-        <div>MetaverseDetails</div>
-    )
+  const [metaverse, setMetaverse] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const fetchMetaverse = useCallback(async () => {
+    const entity = await get({ id });
+
+    if (!entity) {
+      return navigate("/error-404");
+    }
+
+    setMetaverse(entity);
+  }, [id, navigate]);
+
+  useState(() => {
+    fetchMetaverse();
+  }, [fetchMetaverse]);
+
+  return (
+    <>
+      <Title title={"metaverse"} />
+      {!metaverse ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Box
+            sx={{
+              width: "100%",
+              height: "500px",
+              background: `url(${metaverse?.thumbnail})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "100% 500px",
+              backgroundPosition: "center",
+            }}
+          >
+            <Box
+              sx={{
+                background: "rgba(0,0,0,0.3)",
+                width: "100%",
+                height: "100%",
+              }}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              flexDirection={"column"}
+              gap={"50px"}
+            >
+              <Typography
+                textAlign={"center"}
+                variant="h1"
+                color={"#FFFFFF"}
+                fontWeight={"600"}
+              >
+                {metaverse?.name}
+              </Typography>
+            </Box>
+          </Box>
+          <Box p={"30px"}>
+            <Typography variant="h3">Description</Typography>
+            <Typography
+              variant="h6"
+              dangerouslySetInnerHTML={{ __html: metaverse?.description }}
+            />
+          </Box>
+          <Box borderTop={"1px solid black"}>
+            <Footer />
+          </Box>
+        </>
+      )}
+    </>
+  );
 }
